fa0dc09947636a19bf35168f557dd507b495d6d8
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-05-07 23:25:58 -0300

    adjusting database connection

--- Repository/db.js
+++ Repository/db.js
@@ -1,5 +1,6 @@
 import pg from "pg";
 const { Pool } = pg;
+import "dotenv/config";
 
 // It is like a pool of connection. Instead of any time I need a connection I made a request, I will just use one of the connections here on the Pool
 const pool = new Pool({
@@ -8,9 +9,10 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  max: 10,
-  idleTimeoutMillis: 10000,
-  connectionTimeoutMillis: 2000,
 });
+
+// console.log(process.env.DB_HOST);
+console.log((await pool.query("SELECT * FROM users")).rows);
+// console.log(pool);
 
 export default pool;
